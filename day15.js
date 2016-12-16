a0d48b316636faa041576383e3f473af120f2e20
Markku Marjoneva <markku.marjoneva@gmail.com>
2016-12-16 08:41:35 +0200

    Day 15 part 2

--- day15.js
+++ day15.js
@@ -40,7 +40,7 @@ const discs = input.split("\n").map(row => {
   }
 })
 
-function checkPart1(time, discs) {
+function drop(time, discs) {
   for (disc of discs) {
     if ((time + disc.n + disc.pos) % disc.len) return false
   }
@@ -48,6 +48,20 @@ function checkPart1(time, discs) {
 }
 
 let time = 0;
-while (!checkPart1(time, discs)) time++
-
+while (!drop(time, discs)) time++
 console.log('Part 1:', time)
+
+/*
+--- Part Two ---
+
+After getting the first capsule (it contained a star! what great fortune!), the machine detects your success and begins to rearrange itself.
+
+When it's done, the discs are back in their original configuration as if it were time=0 again, but a new disc with 11 positions and starting at position 0 has appeared exactly one second below the previously-bottom disc.
+
+With this new disc, and counting again starting from time=0 with the configuration in your puzzle input, what is the first time you can press the button to get another capsule?
+*/
+
+discs.push({n: 7, len: 11, pos: 0})
+time = 0;
+while (!drop(time, discs)) time++
+console.log('Part 2:', time)
